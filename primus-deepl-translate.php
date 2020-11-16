aa8fde30bf7ec550f6a3b403221679d226c8b553
Nathan G-Michael <info@denade-media.com>
2020-11-16 05:17:15 +0100

    reverted experiments

--- primus-deepl-translate.php
+++ primus-deepl-translate.php
@@ -3,7 +3,7 @@
  * Plugin Name: primus fremdsprachen DeepL Translator
  * Plugin URI: https://denade-media.com
  * Description: Generates a DeepL Translate link 
- * Version: 0.1.2
+ * Version: 0.1.3
  * Text Domain: primus-deepl-translate
  * Author: Nathan G-Michael
  * Author URI: https://denade-media.com
@@ -16,7 +16,7 @@
         'color' => '#ed9f2d',
         'target' => '_blank'
         ), $atts );
-        $output = '<strong><a href="' . esc_url( $a['link'] ) . esc_attr( urlencode(($a['text'] )); . '" style="font-size: larger; color: ' . esc_attr( $a['color'] ) . ';" target="' . esc_attr($a['target']) . '">' . esc_attr( $a['text'] ) . '</a></strong>';
+        $output = '<strong><a href="' . esc_url( $a['link'] ) . esc_attr( ($a['text'] ); . '" style="font-size: larger; color: ' . esc_attr( $a['color'] ) . ';" target="' . esc_attr($a['target']) . '">' . esc_attr( $a['text'] ) . '</a></strong>';
         return $output;
 }
 
