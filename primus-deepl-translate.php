5c7b0e3b449b17418ce899693ef3dbd32322dda2
D-Jinnz Nathan <djinnz.nathan@gmail.com>
2020-11-16 23:19:07 +0100

    Changed text for content attr for closing brackets

--- primus-deepl-translate.php
+++ primus-deepl-translate.php
@@ -3,20 +3,19 @@
  * Plugin Name: primus fremdsprachen DeepL Translator
  * Plugin URI: https://denade-media.com
  * Description: Generates a DeepL Translate link 
- * Version: 0.1.3
+ * Version: 0.1.4
  * Text Domain: primus-deepl-translate
  * Author: Nathan G-Michael
  * Author URI: https://denade-media.com
  */
  
- function primus_deepl_translate($atts) {
+ function primus_deepl_translate($atts, $content) {
     $a = shortcode_atts( array(
         'link' => 'https://www.deepl.com/en/translator#de/en/',
-        'text' => 'Test',
         'color' => '#ed9f2d',
         'target' => '_blank'
         ), $atts );
-        $output = '<strong><a href="' . esc_url( $a['link'] ) . esc_attr( $a['text'] ) . '" style="font-size: larger; color: ' . esc_attr( $a['color'] ) . ';" target="' . esc_attr($a['target']) . '">' . esc_attr( $a['text'] ) . '</a></strong>';
+        $output = '<strong><a href="' . esc_url( $a['link'] ) . esc_url( $content ) . '" style="font-size: larger; color: ' . esc_attr( $a['color'] ) . ';" target="' . esc_attr($a['target']) . '">' . esc_attr( $content ) . '</a></strong>';
         return $output;
 }
 
