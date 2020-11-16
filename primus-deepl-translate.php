2769a72b95f9ec89f9944d3434b1ffde1051200d
D-Jinnz Nathan <djinnz.nathan@gmail.com>
2020-11-16 23:57:33 +0100

    Added emoji at the end and hard coded target

--- primus-deepl-translate.php
+++ primus-deepl-translate.php
@@ -3,7 +3,7 @@
  * Plugin Name: primus fremdsprachen DeepL Translator
  * Plugin URI: https://denade-media.com
  * Description: Generates a DeepL Translate link 
- * Version: 0.1.4
+ * Version: 0.1.5
  * Text Domain: primus-deepl-translate
  * Author: Nathan G-Michael
  * Author URI: https://denade-media.com
@@ -12,12 +12,13 @@
  function primus_deepl_translate($atts, $content) {
     $a = shortcode_atts( array(
         'link' => 'https://www.deepl.com/en/translator#de/en/',
-        'color' => '#ed9f2d',
-        'target' => '_blank'
+        'color' => '#ed9f2d'
         ), $atts );
-        $output = '<strong><a href="' . esc_url( $a['link'] ) . esc_url( $content ) . '" style="font-size: larger; color: ' . esc_attr( $a['color'] ) . ';" target="' . esc_attr($a['target']) . '">' . esc_attr( $content ) . '</a></strong>';
+        $output = '<strong><a href="' . esc_url( $a['link'] ) . esc_attr( $content ) . '" style="font-size: larger; color: ' . esc_attr( $a['color'] ) . ';" target="_blank">' . esc_attr( $content ) . " 🌐" . '</a></strong>';
         return $output;
 }
 
 add_shortcode('DeepL', 'primus_deepl_translate');
+
+
  
