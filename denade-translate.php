4281da79f4ba2c18e482d9b376dc62d3bb0237de
Nathan G-Michael <info@denade-media.com>
2020-11-17 22:26:46 +0100

    Added services and changed readme

--- denade-translate.php
+++ denade-translate.php
@@ -13,18 +13,65 @@
  * License URI:         https://www.gnu.org/licenses/gpl-3.0.html
  */
  
-//   DeepL Service
+//   DeepL 
 
  function denade_deepl_translate($atts, $content) {
     $a = shortcode_atts( array(
         'link' => 'https://www.deepl.com/en/translator#de/en/',
-        'color' => '#ed9f2d'
+        'color' => '#ed9f2d',
+        'sl' => 'de',
+        'tl' => 'en'
         ), $atts );
-        $output = '<strong><a href="' . esc_url( $a['link'] ) . esc_attr( $content ) . '" style="font-size: larger; color: ' . esc_attr( $a['color'] ) . ';" target="_blank">' . esc_attr( $content ) . " 🌐" . '</a></strong>';
+        $output = '<strong><a href="https://www.deepl.com/en/translator#' . esc_attr( $sl) . '/' . esc_attr( $tl ) . '/' . esc_attr( $content ) . '" style="font-size: larger; color: ' . esc_attr( $a['color'] ) . ';" target="_blank">' . esc_attr( $content ) . " 🌐" . '</a></strong>';
         return $output;
 }
 
 add_shortcode('DeepL', 'denade_deepl_translate');
 
 
- 
+//   Google Translator 
+
+function denade_google_translate($atts, $content) {
+    $a = shortcode_atts( array(
+        'link' => 'https://translate.google.com/?hl=en#view=home&op=translate&sl=auto&tl=en&text=',
+        'color' => '#ed9f2d',
+        'sl' => 'auto',
+        'tl' => 'en'
+        ), $atts );
+        $output = '<strong><a href="https://translate.google.com/?hl=en#view=home&op=translate&sl=' . esc_attr( $sl) . '&tl=e' . esc_attr( $tl ) . '&text=' . esc_attr( $content ) . '" style="font-size: larger; color: ' . esc_attr( $a['color'] ) . ';" target="_blank">' . esc_attr( $content ) . " 🌐" . '</a></strong>';
+        return $output;
+}
+
+add_shortcode('GTranslate', 'denade_google_translate');
+
+
+//   Yandex Translate 
+
+function denade_yandex_translate($atts, $content) {
+    $a = shortcode_atts( array(
+        'link' => 'https://translate.yandex.com/?lang=de-ru&text=',
+        'color' => '#ed9f2d',
+        'sl' => 'de',
+        'tl' => 'en'
+        ), $atts );
+        $output = '<strong><a href="https://translate.yandex.com/?lang=' . esc_attr( $sl) . '-' . esc_attr( $tl ) . '&text=' . esc_attr( $content ) . '" style="font-size: larger; color: ' . esc_attr( $a['color'] ) . ';" target="_blank">' . esc_attr( $content ) . " 🌐" . '</a></strong>';
+        return $output;
+}
+
+add_shortcode('Yandex', 'denade_yandex_translate');
+
+
+//   Baidu Translator 
+
+function denade_baidu_translate($atts, $content) {
+    $a = shortcode_atts( array(
+        'link' => 'https://fanyi.baidu.com/#de/zh/',
+        'color' => '#ed9f2d',
+        'sl' => 'de',
+        'tl' => 'en'
+        ), $atts );
+        $output = '<strong><a href="https://fanyi.baidu.com/#' . esc_attr( $sl) . '/' . esc_attr( $tl ) . '/' . esc_attr( $content ) . '" style="font-size: larger; color: ' . esc_attr( $a['color'] ) . ';" target="_blank">' . esc_attr( $content ) . " 🌐" . '</a></strong>';
+        return $output;
+}
+
+add_shortcode('Baidu', 'denade_baidu_translate');
