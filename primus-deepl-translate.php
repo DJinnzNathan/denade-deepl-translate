b4334fee59ae717d2ac0b58bc23921e3b4582ca0
D-Jinnz Nathan <djinnz.nathan@gmail.com>
2020-11-17 02:31:11 +0100

    Changed the license

--- primus-deepl-translate.php
+++ primus-deepl-translate.php
@@ -8,8 +8,8 @@
  * Author:              Nathan G-Michael
  * Requires at least:   5.2
  * Requires PHP:        7.2
- * License:             GPL v2 or later
- * License URI:         https://www.gnu.org/licenses/gpl-2.0.html
+ * License:             GPL v3 or later
+ * License URI:         https://www.gnu.org/licenses/gpl-3.0.html
  */
  
  function primus_deepl_translate($atts, $content) {
